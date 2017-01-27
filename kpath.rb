e98f288938e8c994cc969d8c43f1380a1ea7777c
App Academy Student <student@appacademy.io>
2017-01-27 15:21:02 -0800

    Make the whole thing work

--- kpath.rb
+++ kpath.rb
@@ -1,8 +1,9 @@
 require_relative 'ptnode'
+require 'byebug'
 
 class KnightPathFinder
 
-  attr_accessor :start_pos
+  attr_accessor :start_pos, :current_pos
 
   DELTAS = [
     [2, -1],
@@ -17,6 +18,8 @@ class KnightPathFinder
   def initialize(start_pos)
     @start_pos = start_pos
     @root = PolyTreeNode.new(start_pos)
+    @seen_places = []
+    @current_pos = @start_pos
   end
 
   def valid_pos?(pos)
@@ -24,7 +27,7 @@ class KnightPathFinder
   end
 
   def generate_moves
-    row, col = @start_pos
+    row, col = @current_pos
     moves = []
     DELTAS.each do |dx, dy|
       new_pos = [row + dx, col + dy]
@@ -35,10 +38,36 @@ class KnightPathFinder
 
   def build_children(parent)
     self.generate_moves.map do |move|
-      parent.children << PolyTreeNode.new(move)
+      unless @seen_places.include?(move)
+        parent.children << PolyTreeNode.new(move)
+      end
     end
     parent.children.each { |child| child.parent = parent }
   end
+
+  def find_path(final_pos)
+    queue = [@root]
+    until queue.empty?
+      current_node = queue.shift
+      @current_pos = current_node.value
+      @seen_places << current_node.value
+      break if current_node.value == final_pos
+      queue += build_children(current_node)
+    end
+    trace_back_path(current_node).reverse
+  end
+
+  def trace_back_path(node)
+    path = []
+    until node == nil
+      path << node.value
+      node = node.parent
+    end
+    path
+  end
+
+
+
 
   private
 
